70b83b348c10b0481be7ff2686190f0d117bdfe5
ALEXEI MAZIAR <a.mathjar@gmail.com>
2024-12-01 16:01:03 +0700

    Implement map raftState

--- RafRaft.Domain/IRaftStateMachine.cs
+++ RafRaft.Domain/IRaftStateMachine.cs
@@ -15,10 +15,4 @@ public interface IRaftStateMachine<TDataIn, TDataOut>
    /// <param name="param">Limits, filters or specifies the information to retrieve.</param>
    /// <returns>A struct containing the specified representation of the state machine</returns>
    TDataOut RequestData(string param);
-
-   /// <summary>
-   /// Used to determine the broadcast time.
-   /// The method must implement a dummy request to the state machine, in order to correctly approximate the broadcast time.
-   /// </summary>
-   void TestConnection();
 }
